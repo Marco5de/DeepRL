609a0f7a27beb55286e75c6d08d3ffac27f92291
MarcoDeuscher <marco.deuscher@uni-ulm.de>
2021-11-01 18:43:03 +0100

    working AI gym setup and successful test of torch + CartPole Env

--- src/test01.py
+++ src/test01.py
@@ -1,19 +1,19 @@
 import gym
 
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import PPO2
+from stable_baselines3 import A2C
+from stable_baselines3 import PPO
 
 env = gym.make('CartPole-v1')
-# Optional: PPO2 requires a vectorized environment to run
-# the env is now wrapped automatically when passing it to the constructor
-# env = DummyVecEnv([lambda: env])
 
-model = PPO2(MlpPolicy, env, verbose=1)
+# model = A2C('MlpPolicy', env, verbose=1)
+model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=10000)
+
 
 obs = env.reset()
 for i in range(1000):
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
+    action, _state = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
     env.render()
+    if done:
+      obs = env.reset()
