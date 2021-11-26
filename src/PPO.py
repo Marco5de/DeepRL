3283a80779e8774b99e8bd3df7009fe89478b433
Marco <marco.deuscher@uni-ulm.de>
2021-11-26 21:00:40 +0100

    completed implementation of sampling data into rollout buffer

--- src/PPO.py
+++ src/PPO.py
@@ -2,15 +2,32 @@ import torch
 import torch.nn as nn
 import gym
 
+from src.ANN import PolicyNetwork, ValueFunctionNetwork
+
+
 class PPO(nn.Module):
 
-    def __init__(self, gym_name):
+    def __init__(self, gym_name: str, render_env: bool):
         super().__init__()
         self.env = gym.make(gym_name)
 
+        self.render_env = render_env
+
+        self.state_space_dim = 10
+        self.action_space_dim = 3
+        self.cov_mat = torch.tensor([[1.0, 0, 0],
+                                     [0, 1.0, 0],
+                                     [0, 0, 1.0]])
+
+        # todo: dont hardcode dims and so on
+        self.policy_network = PolicyNetwork(input_dim=self.state_space_dim,
+                                            output_dim=self.action_space_dim,
+                                            covariance_mat=self.cov_mat)
+
+        self.value_func_network = ValueFunctionNetwork(input_dim=self.state_space_dim)
+
     def forward(self):
         pass
-
 
     def evaluate_policy(self, observation: torch.Tensor):
         """
@@ -18,30 +35,40 @@ class PPO(nn.Module):
         Is used for the on-policy algorithm
 
         :param observation last observation that was observed from the env, used to evaluate pi(a|s)
-        :return: action sampled from the current policy
+        :return: action sampled from the current policy and its log probability
         """
-        # mean = NNactor(observation)
-        # distribution = MultiGaussian(mean, covMat)
-        # sample
-        # logprobs
-        # todo: for now return random action
-        return self.env.action_space.sample()
+        action_sample, log_prob = self.policy_network(observation)
+        return action_sample, log_prob
 
     def rollout_buffer(self, N: int, T: int):
-        # todo: alle werte irgendwo speichern und dann wieder rausgeben
-
         rollout_buffer = RolloutBuffer()
 
         for actor in range(N):
             observation = self.env.reset()
+            rollout_buffer.observations.append(observation)
+
+            # episode of length T
+            length = 0
             for t in range(T):
+                if self.render_env:
+                    self.env.render()
+
                 # get action - implement action getter from current policy
-                action = self.eval_policy()
+                action, log_prob = self.eval_policy()
+                rollout_buffer.actions.append(action)
+                rollout_buffer.log_probabilities.append(log_prob)
 
-                observation, action, done, info = self.env.step(action)
-                print(observation)
-                pass
+                observation, reward, done, info = self.env.step(action)
+                rollout_buffer.rewards.append(reward)
 
+                length = t
+                if done:  # if environment sequence is over break from this loop!
+                    break
+
+            # mark length of the episode and the final reward
+            rollout_buffer.episode_length.append(length)
+
+        return rollout_buffer
 
 
 class RolloutBuffer:
@@ -52,9 +79,10 @@ class RolloutBuffer:
     def __init__(self):
         # todo add missing values that must be tracked
         self.actions = []
+        self.log_probabilities = []
         self.observations = []
         self.rewards = []
-
+        self.episode_length = []
 
 
 def __main__():
