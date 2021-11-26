77247591a1c74d62216b44ca11a1ad64cdd2543d
Marco <marco.deuscher@uni-ulm.de>
2021-11-26 22:02:01 +0100

    outline of implementation complete

--- src/PPO.py
+++ src/PPO.py
@@ -1,20 +1,32 @@
 import torch
 import torch.nn as nn
+import torch.optim
 import gym
 
 from src.ANN import PolicyNetwork, ValueFunctionNetwork
 
 
-class PPO(nn.Module):
+class Hyperparameter:
+    def __init__(self):
+        self.epsilon_clip = 0.1  # todo find value
 
-    def __init__(self, gym_name: str, render_env: bool):
+
+class PPO(nn.Module):
+    _valid_surrogate_objectives = ["clipped", "adaptive_KL", "policy_gradient"]
+
+    def __init__(self, gym_name: str, surrogate_objective: str, render_env: bool, base_lr: float):
         super().__init__()
+        assert surrogate_objective in self._valid_surrogate_objectives, "Specified surrogate objective is not valid!"
         self.env = gym.make(gym_name)
 
         self.render_env = render_env
 
+        self.surrogate_objective = surrogate_objective
+        self.hyperparameter = Hyperparameter()
+
         self.state_space_dim = 10
         self.action_space_dim = 3
+        # todo: see paper how cov_mat is initialized
         self.cov_mat = torch.tensor([[1.0, 0, 0],
                                      [0, 1.0, 0],
                                      [0, 0, 1.0]])
@@ -23,11 +35,84 @@ class PPO(nn.Module):
         self.policy_network = PolicyNetwork(input_dim=self.state_space_dim,
                                             output_dim=self.action_space_dim,
                                             covariance_mat=self.cov_mat)
+        self.policy_network_optimizer = torch.optim.Adam(self.policy_network.parameters(), lr=base_lr)
 
         self.value_func_network = ValueFunctionNetwork(input_dim=self.state_space_dim)
+        self.value_func_network_optimizer = torch.optim.Adam(self.value_func_network.parameters(), lr=base_lr)
 
     def forward(self):
-        pass
+        """
+        Learning is done in here, this function must be called repeatedly for #training_steps
+        :return:
+        """
+        # sample data - Run policy π_θ_old in environment for T time steps using N actors
+        # todo parameter for N and T
+        rollout_buffer = self.sample_data(N=10, T=10)
+        actions, log_probs, observations, rewards, episode_length = rollout_buffer.to_tensor()
+
+        # compute advantage estimates A_1, \dots, A_T
+        value, log_probs = self.evaluate_value_function(observations, actions)
+        # todo: trick to normalize advantage functions
+        advtange_values = self.compute_advantage_values(value, rewards)
+
+        # Optimize surrogate L w.r.t. θ with K epochs and minibatch size M ≤ N T
+        self.optimize_neural_networks(observations, actions, K=10)
+
+    def optimize_neural_networks(self, observations, actions, K):
+        """
+        Optimize surrogate L w.r.t. θ with K epochs and minibatch size M ≤ N T
+        :return:
+        """
+        for i in range(K):
+            value, log_probs = self.evaluate_value_function(observations,
+                                                            actions)  # todo does this value change in the loop?! computed earlier
+
+            # todo compute ratio r_t(\theta) im paper
+
+            # compute surrogate objective function
+            surrogate = 0.0
+            if self.surrogate_objective == "clipped":
+                surrogate = self.clipped_surrogate_function()
+            elif self.surrogate_objective == "adaptive_KL":
+                surrogate = self.adaptive_KL_surrogate_function()
+            elif self.surrogate_objective == "policy_gradient":
+                surrogate = self.policy_gradient_surrogate_funcion()
+
+            # todo compute policy_net_loss and value_function_net loss
+            policy_net_loss = None
+            self.policy_network_optimizer.zero_grad()
+            policy_net_loss.backward()
+            self.policy_network_optimizer.step()
+
+            value_function_net_loss = None
+            self.value_func_network_optimizer.zero_grad()
+            value_function_net_loss.backward()
+            self.value_func_network_optimizer.step()
+
+    def compute_advantage_values(self, value: torch.Tensor, rewards: torch.Tensor):
+        """
+        Paper specifies a couple of estimators are they equivalent?
+        Implementations contain other, different estimators, which one to use?
+
+        :param value:
+        :param rewards:
+        :return:
+        """
+        return None
+
+    def evaluate_value_function(self, observations: torch.Tensor, actions: torch.Tensor):
+        """
+        todo was wird hier genau berechnet, ist das V(s) oder Q(a,s)
+        :param observations:
+        :param actions:
+        :return:
+        """
+        value = self.value_func_network(observations)
+
+        # todo hier ist die implementierung vom policy network doof, weil ich dierekt an die Verteilung muss... Läsug überlegen
+        log_probs = None
+
+        return value, log_probs
 
     def evaluate_policy(self, observation: torch.Tensor):
         """
@@ -40,7 +125,7 @@ class PPO(nn.Module):
         action_sample, log_prob = self.policy_network(observation)
         return action_sample, log_prob
 
-    def rollout_buffer(self, N: int, T: int):
+    def sample_data(self, N: int, T: int):
         rollout_buffer = RolloutBuffer()
 
         for actor in range(N):
@@ -70,6 +155,35 @@ class PPO(nn.Module):
 
         return rollout_buffer
 
+    def clipped_surrogate_function(self, old, new, advantage_values):
+        """
+        Implements the clipped surrogate function
+        PPO paper section (3)
+        :return: value of clipped surrogate function
+        """
+        r_t = torch.exp(new) / torch.exp(old)
+
+        surrogate = torch.min(torch.tensor([
+            r_t * advantage_values,
+            torch.clamp(r_t, 1 - self.hyperparameter.epsilon_clip, 1 + self.hyperparameter.epsilon_clip)
+        ]))
+
+        return surrogate
+
+    def adaptive_KL_surrogate_function(self):
+        """
+        todo: implement
+        :return:
+        """
+        return 0
+
+    def policy_gradient_surrogate_funcion(self):
+        """
+        todo: implement
+        :return:
+        """
+        return 0
+
 
 class RolloutBuffer:
     """
@@ -77,12 +191,25 @@ class RolloutBuffer:
     """
 
     def __init__(self):
-        # todo add missing values that must be tracked
         self.actions = []
         self.log_probabilities = []
         self.observations = []
         self.rewards = []
         self.episode_length = []
+
+    def to_tensor(self):
+        """
+        Conversion from python lists to torch.Tensor
+
+        :return: actions, log_probabilities, observations, rewards, episode length as torch.Tensor
+        """
+        tensor_actions = torch.tensor(self.actions)
+        tensor_log_probs = torch.tensor(self.log_probabilities)
+        tensor_observations = torch.tensor(self.observations)
+        tensor_rewards = torch.tensor(self.rewards)
+        tensor_episode_length = torch.tensor(self.episode_length)
+
+        return tensor_actions, tensor_log_probs, tensor_observations, tensor_rewards, tensor_episode_length
 
 
 def __main__():
