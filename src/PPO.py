5d3d03acca2ae663340781f62860364cf0da0bf9
Marco <marco.deuscher@uni-ulm.de>
2021-11-30 11:26:23 +0100

    untested implementation of PPO complete

--- src/PPO.py
+++ src/PPO.py
@@ -2,14 +2,51 @@ import torch
 import torch.nn as nn
 import torch.optim
 import gym
+from typing import Tuple
 
 from src.ANN import PolicyNetwork, ValueFunctionNetwork
+from src.util import kullback_leibler_div
 
 
 class Hyperparameter:
     def __init__(self):
-        self.epsilon_clip = 0.1  # todo find value
+        # todo find value from paper / impl
+        self.epsilon_clip = 0.1
         self.gamma = 0.95
+        self.beta = 1.0
+        self.d_target_ratio = 1.5
+        self.d_target = 0.1
+        self.var = 1.0
+        self.N = 10
+        self.T = 10
+        self.K = 5
+
+
+class RolloutBuffer:
+    """
+    Implementation of a rollout buffer which is filled when sampling data from the environment in the PPO algorithm
+    """
+
+    def __init__(self):
+        self.actions = []
+        self.log_probabilities = []
+        self.observations = []
+        self.rewards = []
+        self.episode_length = []
+
+    def to_tensor(self):
+        """
+        Conversion from python lists to torch.Tensor
+
+        :return: actions, log_probabilities, observations, rewards, episode length as torch.Tensor
+        """
+        tensor_actions = torch.tensor(self.actions)
+        tensor_log_probs = torch.tensor(self.log_probabilities)
+        tensor_observations = torch.tensor(self.observations)
+        tensor_rewards = torch.tensor(self.rewards)
+        tensor_episode_length = torch.tensor(self.episode_length)
+
+        return tensor_actions, tensor_log_probs, tensor_observations, tensor_rewards, tensor_episode_length
 
 
 class PPO(nn.Module):
@@ -25,14 +62,11 @@ class PPO(nn.Module):
         self.surrogate_objective = surrogate_objective
         self.hyperparameter = Hyperparameter()
 
-        self.state_space_dim = 10
-        self.action_space_dim = 3
-        # todo: see paper how cov_mat is initialized
-        self.cov_mat = torch.tensor([[1.0, 0, 0],
-                                     [0, 1.0, 0],
-                                     [0, 0, 1.0]])
+        self.state_space_dim = self.env.observation_space.n
+        self.action_space_dim = self.env.action_space.n
+        # todo: macht es sinn, dass die covariance für alle dimensionen gleich ist?
+        self.cov_mat = torch.diag(torch.tensor([self.hyperparameter.var] * self.state_space_dim))
 
-        # todo: dont hardcode dims and so on
         self.policy_network = PolicyNetwork(input_dim=self.state_space_dim,
                                             output_dim=self.action_space_dim,
                                             covariance_mat=self.cov_mat)
@@ -41,13 +75,12 @@ class PPO(nn.Module):
         self.value_func_network = ValueFunctionNetwork(input_dim=self.state_space_dim)
         self.value_func_network_optimizer = torch.optim.Adam(self.value_func_network.parameters(), lr=base_lr)
 
-    def forward(self):
+    def forward(self) -> None:
         """
         Learning is done in here, this function must be called repeatedly for #training_steps
         :return:
         """
         # sample data - Run policy π_θ_old in environment for T time steps using N actors
-        # todo parameter for N and T
         rollout_buffer = self.sample_data(N=10, T=10)
         actions, log_probs, observations, rewards, episode_length = rollout_buffer.to_tensor()
 
@@ -59,7 +92,7 @@ class PPO(nn.Module):
         # Optimize surrogate L w.r.t. θ with K epochs and minibatch size M ≤ N T
         self.optimize_neural_networks(observations, actions, K=10)
 
-    def optimize_neural_networks(self, observations, actions, K):
+    def optimize_neural_networks(self, observations, actions, K) -> None:
         """
         Optimize surrogate L w.r.t. θ with K epochs and minibatch size M ≤ N T
         :return:
@@ -67,8 +100,6 @@ class PPO(nn.Module):
         for i in range(K):
             value, log_probs = self.evaluate_value_function(observations,
                                                             actions)  # this expression changes as the NN is updated!
-
-            # todo compute ratio r_t(\theta) im paper
 
             # compute surrogate objective function
             surrogate = 0.0
@@ -79,45 +110,50 @@ class PPO(nn.Module):
             elif self.surrogate_objective == "policy_gradient":
                 surrogate = self.policy_gradient_surrogate_funcion()
 
-            # todo compute policy_net_loss and value_function_net loss
-            policy_net_loss = None
+            policy_net_loss = - surrogate # maximization!
             self.policy_network_optimizer.zero_grad()
             policy_net_loss.backward()
             self.policy_network_optimizer.step()
 
+            # todo: was nimmt man also Value function loss?
             value_function_net_loss = None
             self.value_func_network_optimizer.zero_grad()
             value_function_net_loss.backward()
             self.value_func_network_optimizer.step()
 
-    def compute_advantage_values(self, value: torch.Tensor, rewards: torch.Tensor, episode_lengths: torch.Tensor):
+    def compute_advantage_values(self, value: torch.Tensor, rewards: torch.Tensor, episode_lengths: torch.Tensor) \
+            -> torch.Tensor:
         """
         Paper specifies a couple of estimators are they equivalent?
         Implementations contain other, different estimators, which one to use?
 
         :param value:
         :param rewards:
-        :return:
+        :param episode_lengths:
+        :return: advantage values torch.Tensor
         """
-        # todo ich muss hier dann mal durch meine rewards durch
+        # todo: lese https://danieltakeshi.github.io/2017/04/02/notes-on-the-generalized-advantage-estimation-paper/
         number_of_episodes = episode_lengths.size()
 
-        # todo: backwards ist einfach.. Berechnung ist mir noch nicht so ganz klar
-        idx_offset = 0
-        for episode_length in episode_lengths:
-            print("Episode length: ", episode_length)
+        # todo check if this index magic is working!
+        discounted_rewards = []
+        offset = 0
+        for i in range(number_of_episodes):
+            sum = 0
+            length = episode_lengths[-(i + 1)]  # reverse
+            for j in range(length):
+                # length is the length of the current episode
+                sum = rewards[-(j + offset + 1)] + self.hyperparameter.gamma * sum
+                discounted_rewards.insert(0, sum)
+            offset += length
+        discounted_rewards = torch.tensor(discounted_rewards)
 
-            for i in range(episode_lengths):
-                current_reward = rewards[idx_offset + i]
+        return discounted_rewards - value
 
-
-            idx_offset += episode_length
-
-        return None
-
-    def evaluate_value_function(self, observations: torch.Tensor, actions: torch.Tensor):
+    def evaluate_value_function(self, observations: torch.Tensor, actions: torch.Tensor) \
+            -> Tuple[torch.Tensor, torch.Tensor]:
         """
-        todo was wird hier genau berechnet, ist das V(s) oder Q(a,s)
+        Estimation of the value function V(s) by evaluating the NN
         :param observations:
         :param actions:
         :return:
@@ -128,7 +164,7 @@ class PPO(nn.Module):
 
         return value, log_probs
 
-    def evaluate_policy(self, observation: torch.Tensor):
+    def evaluate_policy(self, observation: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Evaluates the actor policy network to generate an action
         Is used for the on-policy algorithm
@@ -139,7 +175,7 @@ class PPO(nn.Module):
         action_sample, log_prob = self.policy_network(observation)
         return action_sample, log_prob
 
-    def sample_data(self, N: int, T: int):
+    def sample_data(self, N: int, T: int) -> RolloutBuffer:
         rollout_buffer = RolloutBuffer()
 
         for actor in range(N):
@@ -169,7 +205,8 @@ class PPO(nn.Module):
 
         return rollout_buffer
 
-    def clipped_surrogate_function(self, old, new, advantage_values):
+    def clipped_surrogate_function(self, old: torch.Tensor, new: torch.Tensor, advantage_values: torch.Tensor) \
+            -> torch.Tensor:
         """
         Implements the clipped surrogate function
         PPO paper section (3)
@@ -184,47 +221,36 @@ class PPO(nn.Module):
 
         return surrogate
 
-    def adaptive_KL_surrogate_function(self):
+    def adaptive_KL_surrogate_function(self, old: torch.Tensor, new: torch.Tensor,
+                                       advantage_values: torch.Tensor, clip: bool = True) -> torch.Tensor:
+        """
+        :param clip specifies whether the clipping method (see paper and clipped surrogate function) is applied
+        :return:
+        """
+        surrogate = 0.0
+        if clip:
+            surrogate = self.clipped_surrogate_function(old, new, advantage_values)
+        else:
+            r_t = torch.exp(new) / torch.exp(old)
+            surrogate = r_t * advantage_values
+
+        kl_penalty = kullback_leibler_div(old, self.cov_mat, new, self.cov_mat)
+        combined = surrogate - self.hyperparameter.beta * kl_penalty
+        # todo im Paper ist da noch eine Expectation, aber ich denke mal in der Implementierung ist das über den Batch drin
+        # updated beta is used for the next policy update!
+        if kl_penalty < (self.hyperparameter.d_target / self.hyperparameter.d_target_ratio):
+            self.hyperparameter.beta = 0.5 * self.hyperparameter.beta
+        else:
+            self.hyperparameter.beta = 2 * self.hyperparameter.beta
+
+        return combined
+
+    def policy_gradient_surrogate_funcion(self) -> torch.Tensor:
         """
         todo: implement
-        todo: clipping findet normal trotzdem noch statt!
         :return:
         """
         return 0
-
-    def policy_gradient_surrogate_funcion(self):
-        """
-        todo: implement
-        :return:
-        """
-        return 0
-
-
-class RolloutBuffer:
-    """
-    Implementation of a rollout buffer which is filled when sampling data from the environment in the PPO algorithm
-    """
-
-    def __init__(self):
-        self.actions = []
-        self.log_probabilities = []
-        self.observations = []
-        self.rewards = []
-        self.episode_length = []
-
-    def to_tensor(self):
-        """
-        Conversion from python lists to torch.Tensor
-
-        :return: actions, log_probabilities, observations, rewards, episode length as torch.Tensor
-        """
-        tensor_actions = torch.tensor(self.actions)
-        tensor_log_probs = torch.tensor(self.log_probabilities)
-        tensor_observations = torch.tensor(self.observations)
-        tensor_rewards = torch.tensor(self.rewards)
-        tensor_episode_length = torch.tensor(self.episode_length)
-
-        return tensor_actions, tensor_log_probs, tensor_observations, tensor_rewards, tensor_episode_length
 
 
 def __main__():
---
+++ src/util.py
@@ -0,0 +1,22 @@
+import torch
+
+
+def kullback_leibler_div(m1: torch.Tensor, s1: torch.Tensor, m2: torch.Tensor, s2: torch.Tensor) -> torch.Tensor:
+    """
+     Computes the Kullback-Leibler Divergence of two multivariate gaussians D(P_1 || P_2)
+     Implementation based on https://stanford.edu/~jduchi/projects/general_notes.pdf
+
+     :param m1 mean of gaussian 1
+     :param s2 covariance matrix of gaussian 1
+     :param m2 mean of gaussian 2
+     :param s2 covariance matrix of gaussian 2
+     :return Kullback-leiber divergence beetween gaussian 1 and gaussian 2
+    """
+    # todo get dim
+    n = m1.size()
+
+    # todo: check if this implementation is correct
+    kl = 0.5 * (torch.log(torch.det(s2) / torch.det(s1)) - n + torch.trace(torch.inverse(s2) * s1)
+                + (m2 - m1).t() * torch.inverse(s2) * (m2 - m1))
+
+    return kl
