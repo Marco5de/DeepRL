added normalization env and fixed some errors

src/PPO.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 import torch.optim
 import numpy as np
 import gym
-from typing import Tuple
+from typing import Tuple, List
+from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
 
 from src.ANN import PolicyNetwork, ValueFunctionNetwork
 from src.util import kullback_leibler_div
@@ -13,15 +14,16 @@ from src.util import kullback_leibler_div
 class Hyperparameter:
     def __init__(self):
         # todo find value from paper / impl
-        self.epsilon_clip = 0.1
-        self.gamma = 0.95
+        # https://github.com/araffin/rl-baselines-zoo/blob/master/hyperparams/ppo2.yml
+        self.epsilon_clip = 0.2 # cliprange
+        self.gamma = 0.99
         self.beta = 1.0
         self.d_target_ratio = 1.5
         self.d_target = 0.1
         self.var = 1.0
-        self.N = 30
-        self.T = 50
-        self.K = 1
+        self.N = 32 # minibatches
+        self.T = 64 # nsteps
+        self.K = 10 # noptepochs
         self.numeric_stable = 1e-9
 
 
@@ -59,6 +61,19 @@ class PPO(nn.Module):
         super().__init__()
         assert surrogate_objective in self._valid_surrogate_objectives, "Specified surrogate objective is not valid!"
         self.env = gym.make(gym_name)
+        self.state_space_dim = len(self.env.robot.observation_space.high)
+        self.action_space_dim = len(self.env.robot.action_space.high)
+
+        self.action_space_lim = (self.env.robot.action_space.low, self.env.robot.action_space.high)
+        self.state_space_lim = (self.env.robot.observation_space.low, self.env.robot.observation_space.high)
+
+        print(f"Action space limits (low, high)= {self.action_space_lim}\nState space limits (low, high)= "
+              f"{self.state_space_lim}")
+
+        self.env = DummyVecEnv([lambda: gym.make(gym_name)])
+        self.env = VecNormalize(self.env, norm_obs=True, norm_reward=True,
+                                clip_obs=10.0) # todo: was ist für clip ein sinnvoller wert?
+
         self.model_save_dir = model_save_dir
         if not os.path.exists(self.model_save_dir):
             os.makedirs(self.model_save_dir)
@@ -68,15 +83,6 @@ class PPO(nn.Module):
         self.render_env = render_env
         if self.render_env:
             self.env.render()
-
-        self.state_space_dim = len(self.env.robot.observation_space.high)
-        self.action_space_dim = len(self.env.robot.action_space.high)
-
-        self.action_space_lim = (self.env.robot.action_space.low, self.env.robot.action_space.high)
-        self.state_space_lim = (self.env.robot.observation_space.low, self.env.robot.observation_space.high)
-
-        print(f"Action space limits (low, high)= {self.action_space_lim}\nState space limits (low, high)= "
-              f"{self.state_space_lim}")
 
         self.surrogate_objective = surrogate_objective
         self.hyperparameter = Hyperparameter()
@@ -101,29 +107,23 @@ class PPO(nn.Module):
         rollout_buffer = self.sample_data(N=self.hyperparameter.N, T=self.hyperparameter.T)
         actions, log_probs, observations, rewards, episode_length = rollout_buffer.to_tensor()
 
-        # compute advantage estimates A_1, \dots, A_T
-        value, log_probs = self.evaluate_value_function(observations, actions)
-
-        rewards_togo = self.compute_rewards_togo(rewards, episode_length)
-        # graph of values is not required --> detach
-        value = value.detach()
-        advantage_vals = rewards_togo - value.squeeze()
-        # normalize https://datascience.stackexchange.com/questions/20098/why-do-we-normalize-the-discounted-rewards-when-doing-policy-gradient-reinforcem
-        advantage_vals = (advantage_vals - torch.mean(advantage_vals)) / (
-                torch.std(advantage_vals) + self.hyperparameter.numeric_stable)
+        advantage_vals, rewards_togo = self.compute_advantage_values(observations, actions, rewards, episode_length)
 
         # Optimize surrogate L w.r.t. θ with K epochs and minibatch size M ≤ N T
-        self.optimize_neural_networks(observations, actions, log_probs, advantage_vals, rewards_togo,
-                                      K=self.hyperparameter.K)
+        polidcy_net_loss, value_net_loss = self.optimize_neural_networks(observations, actions, log_probs,
+                                                                         advantage_vals, rewards_togo,
+                                                                         K=self.hyperparameter.K)
+        return polidcy_net_loss, value_net_loss
 
-        print("Step comlete")
-
-    def optimize_neural_networks(self, observations, actions, log_probs, advantage_values, rewards_togo, K) -> None:
+    def optimize_neural_networks(self, observations, actions, log_probs, advantage_values, rewards_togo, K) \
+            -> Tuple[float, float]:
         """
         Optimize surrogate L w.r.t. θ with K epochs and minibatch size M ≤ N T
-        todo: hier gibt es probleme mit dem Gradienten graph retain blablabla
         :return:
         """
+        avg_policy_net_loss = 0.0
+        avg_value_net_loss = 0.0
+
         for i in range(K):
             # this expression changes as the NN is updated!
             value, current_log_probs = self.evaluate_value_function(observations, actions)
@@ -145,13 +145,16 @@ class PPO(nn.Module):
             self.policy_network_optimizer.step()
 
             # todo: MSE loss oder huber loss?
-            value_function_net_loss = nn.MSELoss()(value.squeeze(), rewards_togo)
+            # value_function_net_loss = nn.MSELoss()(value.squeeze(), rewards_togo)
+            value_function_net_loss = nn.HuberLoss()(value.squeeze(), rewards_togo)
             self.value_func_network_optimizer.zero_grad()
             value_function_net_loss.backward()
             self.value_func_network_optimizer.step()
 
-            print(f"Policy net loss= {policy_net_loss.detach()}\n"
-                  f"Value Func. loss= {value_function_net_loss.detach()}")
+            avg_policy_net_loss += policy_net_loss.detach()
+            avg_value_net_loss += value_function_net_loss.detach()
+
+        return avg_policy_net_loss, avg_value_net_loss
 
     def compute_rewards_togo(self, rewards: torch.Tensor, episode_lengths: torch.Tensor) \
             -> torch.Tensor:
@@ -163,14 +166,12 @@ class PPO(nn.Module):
         :param episode_lengths:
         :return: advantage values torch.Tensor
         """
-        number_of_episodes = episode_lengths.shape[0]
-
         # todo check if this index magic is working! --> works but correct?
         discounted_rewards = []
         offset = 0
-        for i in range(number_of_episodes):
+        for i in range(self.hyperparameter.N):
             sum = 0
-            length = episode_lengths[-(i + 1)]  # reverse
+            length = episode_lengths[-(i + 1)].item()  # reverse
             for j in range(length):
                 # length is the length of the current episode
                 sum = rewards[-(j + offset + 1)] + self.hyperparameter.gamma * sum
@@ -205,7 +206,35 @@ class PPO(nn.Module):
         action_sample, log_prob = self.policy_network(observation)
         return action_sample, log_prob
 
+    def compute_advantage_values(self, observations: torch.Tensor, actions: torch.Tensor, rewards: torch.Tensor,
+                                 episode_length: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Computes the advantage values and the reward to go
+        :param observations:        observations from the rollout buffer as torch.Tensor
+        :param actions:             actions from the rollout buffer as torch.Tensor
+        :param rewards:             rewards from the rollout buffer as torch.Tensor
+        :param episode_length:      episode length of each episode
+        :return:                    advantage_values and reward-to-go
+        """
+        # compute advantage estimates A_1, \dots, A_T
+        value, _ = self.evaluate_value_function(observations, actions)
+
+        rewards_togo = self.compute_rewards_togo(rewards, episode_length)
+        # graph of values is not required --> detach
+        # value = value.detach()
+        advantage_vals = rewards_togo - value.detach().squeeze()
+        # normalize https://datascience.stackexchange.com/questions/20098/why-do-we-normalize-the-discounted-rewards-when-doing-policy-gradient-reinforcem
+        advantage_vals = (advantage_vals - torch.mean(advantage_vals)) / (
+                torch.std(advantage_vals) + self.hyperparameter.numeric_stable)
+        return advantage_vals, rewards_togo
+
     def sample_data(self, N: int, T: int) -> RolloutBuffer:
+        """
+        Computation of the rollout buffer, sampling data from the environment
+        :param N:   number of actors in parallel / different episodes that are considered
+        :param T:   nubmer of timesteps per episode
+        :return:    RolloutBuffer, includes observation, action, reward, episode_length
+        """
         rollout_buffer = RolloutBuffer()
 
         for actor in range(N):
@@ -240,11 +269,20 @@ class PPO(nn.Module):
         PPO paper section (3)
         :return: value of clipped surrogate function
         """
+
+        """ todo: trying different implementation of surrogate function
         r_t = torch.exp(new) / torch.exp(old)
 
         surrogate = torch.mean(torch.min(r_t * advantage_values,
                                          torch.clamp(r_t, 1 - self.hyperparameter.epsilon_clip,
                                                      1 + self.hyperparameter.epsilon_clip) * advantage_values))
+        """
+        ratios = torch.exp(new - old)
+        surr1 = ratios * advantage_values
+        surr2 = torch.clamp(ratios, 1 - self.hyperparameter.epsilon_clip,
+                            1 + self.hyperparameter.epsilon_clip) * advantage_values
+        surrogate = (-torch.min(surr1, surr2)).mean()
+        # """
         return surrogate
 
     def adaptive_KL_surrogate_function(self, old: torch.Tensor, new: torch.Tensor,
@@ -262,7 +300,6 @@ class PPO(nn.Module):
 
         kl_penalty = kullback_leibler_div(old, self.cov_mat, new, self.cov_mat)
         combined = surrogate - self.hyperparameter.beta * kl_penalty
-        # todo im Paper ist da noch eine Expectation, aber ich denke mal in der Implementierung ist das über den Batch drin
         # updated beta is used for the next policy update!
         if kl_penalty < (self.hyperparameter.d_target / self.hyperparameter.d_target_ratio):
             self.hyperparameter.beta = 0.5 * self.hyperparameter.beta
@@ -279,14 +316,57 @@ class PPO(nn.Module):
         """
         raise NotImplementedError
 
-    def save_model(self):
+    def validation(self, num_episodes) -> Tuple[float, List[int]]:
+        """
+        Perform validation step on env. by evaluating the cuurrent policy and returning the average reward
+        :param num_episodes: number of episodes that are simulated
+        :return: average reward of the episodes and length of the episodes
+        """
+        # don't update env stats and norm for reward is not required during validation
+        self.env.training = False
+        self.env.norm_reward = False
+
+        cum_rewards = 0.0
+        episode_lengths = []
+        for episode in range(num_episodes):
+            obs = self.env.reset()
+            done = False
+            t = 0
+
+            while not done:
+                action, _ = self.evaluate_policy(torch.tensor(obs))
+                obs, reward, done, _ = self.env.step(action.detach().numpy())
+                cum_rewards += reward
+                t += 1
+
+            episode_lengths.append(t)
+
+        # training will be resumed and for that normalization is expected
+        self.env.training = True
+        self.env.norm_reward = True
+        return cum_rewards / num_episodes, episode_lengths
+
+    def save_model(self) -> None:
+        """
+        Saves the policy and value function network on the specified path
+        self.model_save_dir/policy_net.pth
+        self.model_save_dir/value_net.pth
+        :return: None
+        """
         torch.save(self.policy_network.state_dict(), os.path.join(self.model_save_dir, "policy_net.pth"))
         torch.save(self.value_func_network.state_dict(), os.path.join(self.model_save_dir, "value_net.pth"))
+        self.env.save(os.path.join(self.model_save_dir, "vec_normalize.pkl"))
         print(f"Saved model parameters to {self.model_save_dir}")
 
-    def load_model(self, model_path):
+    def load_model(self, model_path: str) -> None:
+        """
+        Load model located at model_path/policy_net.pth and model_path/value_net.pth
+        :param model_path: path to the model directory
+        :return: None
+        """
         policy_net_path = os.path.join(model_path, "policy_net.pth")
         value_net_path = os.path.join(model_path, "value_net.pth")
-
+        env_stats = os.path.join(model_path, "vec_normalize.pkl")
+        self.env = VecNormalize.load(env_stats, self.env)
         self.policy_network.load_state_dict(torch.load(policy_net_path))
         self.value_func_network.load_state_dict(torch.load(value_net_path))

src/eval.py
@@ -1,14 +1,13 @@
 import os, time
 from datetime import datetime
 import gym
-import pybulletgym
 import torch
 
 from src.PPO import PPO
 from src.train import ENV_NAMES
 
-RENDER_ENV = True
-MODEL_DIR_PATH = "res/model/model_06_01_22-13_03_1641470638"
+RENDER_ENV = False
+MODEL_DIR_PATH = "res/model/model_06_01_22-13_43_1641473027"
 NUM_EPISODES = 100
 
 
@@ -23,7 +22,6 @@ def eval():
     env = gym.make(ENV_NAMES[0])
 
     cum_rewards = 0.0
-    step = 0
 
     if RENDER_ENV:
         env.render()
@@ -33,23 +31,13 @@ def eval():
 
         done = False
         while not done:
-            action, log_probs = agent.evaluate_policy(torch.tensor(obs))
-            obs, reward, done, info = env.step(action)
+            action, _ = agent.evaluate_policy(torch.tensor(obs))
+            obs, reward, done, info = env.step(action.detach().numpy())
             cum_rewards += reward
-            step += 1
-            time.sleep(0.01)
+            if RENDER_ENV:
+                time.sleep(0.01)
 
-        print(f"Average reward= {cum_rewards / step}")
-
-
-
-
-
-
-
-
-
-
+    print(f"Average reward= {cum_rewards / NUM_EPISODES}")
 
 
 if __name__ == "__main__":